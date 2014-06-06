76599437b2d5d270169661b197f2fc3524805236
infosecsmith <infosecsmith@gmail.com>
2014-06-06 00:03:43 -0600

    Update SSL-Server.ps1

--- SSL-Server.ps1
+++ SSL-Server.ps1
@@ -127,8 +127,8 @@ if ($sslcertfake -eq $null)
 	$sslcertfake =  createCertificate  $ListenerIP $false
 }
 $sslThumbprint = $sslcertfake.Thumbprint 
-$installCert = "netsh http add sslcert ipport=0.0.0.0:8082 certhash=$sslThumbprint appid={e46ad221-627f-4c05-9bb6-2529ae1fa815}"
-$installCert
+$installCert = "netsh http add sslcert ipport=0.0.0.0:8082 certhash=$sslThumbprint appid='{e46ad221-627f-4c05-9bb6-2529ae1fa815}'"
+iex $installCert
 'SSL Certificates Installed...'
 $listener.Prefixes.Add('https://+:8082/') #HTTPS Listener
 
